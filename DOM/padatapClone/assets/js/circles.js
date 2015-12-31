b4b37c1be77bdd283e791e1363a0b3dd055516da
Bryan Kahler <bryankahler@Bryans-MacBook-Air.local>
2015-12-31 11:27:08 -0800

    finished mini padatap clone

--- DOM/padatapClone/assets/js/circles.js
+++ DOM/padatapClone/assets/js/circles.js
@@ -1,17 +1,106 @@
-		// Create a Paper.js Path to draw a line into it:
-		var path = new Path();
-		// Give the stroke a color
-		path.strokeColor = 'red';
-		var start = new Point(100, 100);
-		// Move to start and draw a line from there
-		path.moveTo(start);
-		// Note the plus operator on Point objects.
-		// PaperScript does that for us, and much more!
-		path.lineTo(start + [ 100, -50 ]);
-
-
-		var myCircle = new Path.Circle(new Point(100, 70), 50);
-		myCircle.fillColor = 'black';
-
-		var myCircle = new Path.Circle(new Point(50, 50), 250);
-		myCircle.fillColor = 'purple';
+var keyData = {
+			q: {
+				color: "red",
+				sound: new Howl({urls: ['assets/sounds/A/bubbles.mp3']})
+			},			
+			w: {
+				color: "green",
+				sound: new Howl({urls: ['assets/sounds/A/clay.mp3']})
+			},			
+			e: {
+				color: "blue",
+				sound: new Howl({urls: ['assets/sounds/A/confetti.mp3']})
+			},
+			r: {
+				color: "purple",
+				sound: new Howl({urls: ['assets/sounds/A/corona.mp3']})
+			},
+			t: {
+				color: "orange",
+				sound: new Howl({urls: ['assets/sounds/A/dotted-spiral.mp3']})
+			},		
+			y: {
+				color: "yellow",
+				sound: new Howl({urls: ['assets/sounds/A/flash-1.mp3']})
+			},			
+			u: {
+				color: "pink",
+				sound: new Howl({urls: ['assets/sounds/A/flash-2.mp3']})
+			},			
+			i: {
+				color: "cyan",
+				sound: new Howl({urls: ['assets/sounds/A/flash-3.mp3']})
+			},			
+			o: {
+				color: "LightSeaGreen",
+				sound: new Howl({urls: ['assets/sounds/A/glimmer.mp3']})
+			},			
+			p: {
+				color: "OrangeRed",
+				sound: new Howl({urls: ['assets/sounds/A/moon.mp3']})
+			},			
+			a: {
+				color: "RoyalBlue",
+				sound: new Howl({urls: ['assets/sounds/A/pinwheel.mp3']})
+			},			
+			s: {
+				color: "Thistle",
+				sound: new Howl({urls: ['assets/sounds/A/piston-1.mp3']})
+			},			
+			d: {
+				color: "HotPink",
+				sound: new Howl({urls: ['assets/sounds/A/piston-2.mp3']})
+			},			
+			f: {
+				color: "Khaki",
+				sound: new Howl({urls: ['assets/sounds/A/piston-3.mp3']})
+			},			
+			g: {
+				color: "Brown",
+				sound: new Howl({urls: ['assets/sounds/A/prism-1.mp3']})
+			},			
+			h: {
+				color: "Aqua",
+				sound: new Howl({urls: ['assets/sounds/A/prism-2.mp3']})
+			},			
+			j: {
+				color: "red",
+				sound: new Howl({urls: ['assets/sounds/A/prism-3.mp3']})
+			},			
+			k: {
+				color: "green",
+				sound: new Howl({urls: ['assets/sounds/A/splits.mp3']})
+			},			
+			l: {
+				color: "blue",
+				sound: new Howl({urls: ['assets/sounds/A/squiggle.mp3']})
+			},			
+			z: {
+				color: "yellow",
+				sound: new Howl({urls: ['assets/sounds/A/strike.mp3']})
+			},			
+			x: {
+				color: "orange",
+				sound: new Howl({urls: ['assets/sounds/A/suspension.mp3']})
+			},			
+			c: {
+				color: "purple",
+				sound: new Howl({urls: ['assets/sounds/A/timer.mp3']})
+			},			
+			v: {
+				color: "pink",
+				sound: new Howl({urls: ['assets/sounds/A/ufo.mp3']})
+			},			
+			b: {
+				color: "cyan",
+				sound: new Howl({urls: ['assets/sounds/A/veil.mp3']})
+			},			
+			n: {
+				color: "darkblue",
+				sound: new Howl({urls: ['assets/sounds/A/wipe.mp3']})
+			},			
+			m: {
+				color: "green",
+				sound: new Howl({urls: ['assets/sounds/A/zig-zag.mp3']})
+			}
+		};
