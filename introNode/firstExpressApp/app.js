0e823717b1da19b8bfd06f958d66e14299b006cf
Bryan Kahler <bkahler3@gmail.com>
2016-01-03 18:37:38 +0000

    More simple routing exercises

--- introNode/firstExpressApp/app.js
+++ introNode/firstExpressApp/app.js
@@ -14,6 +14,19 @@ app.get("/cat", function(req, res){
     res.send("Meow");
 });
 
+app.get("/r/:subReddit", function(req, res){
+    console.log(req);
+    res.send("Welcome to the " + req.params.subReddit + " subreddit!");
+});
+
+app.get("/r/:subReddit/comments/:id/:title", function(req, res){
+    res.send("Welcome to the comments page!");
+});
+
+app.get("*", function(req, res){
+    res.send("No Yet Implemented, but we are working on it...");
+});
+
 // Tells Express to Listen on a specified PORT and IP. Call back prints message to console.
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server Has Started...")
