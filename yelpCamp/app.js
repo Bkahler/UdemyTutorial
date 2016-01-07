824287c5c2a4316629d060a3d4cee6bb180450c2
Bryan Kahler <bkahler3@gmail.com>
2016-01-07 04:34:41 +0000

    basic views for yelpcamp

--- yelpCamp/app.js
+++ yelpCamp/app.js
@@ -10,10 +10,15 @@ app.set("view engine", "ejs");
 // shared variables
 var campgrounds = [
     {name:"Salt Point", image:"http://bit.ly/1Uv0iJt"},
-    {name:"Pepper Ridge", image:"http://bit.ly/1Uv0iJt"},
-    {name:"Cinnamon Falls", image:"http://bit.ly/1Uv0iJt"},
+    {name:"Pepper Ridge", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSPRncMF_1mX0P6gPhNgwEz1ZvWQg5DxIClHcHT028n9feGEvbxQ"},
     {name:"Rosemary Lakes", image:"http://bit.ly/1Uv0iJt"},
-    {name:"Sage Fields", image:"http://bit.ly/1Uv0iJt"}
+    {name:"Sage Fields", image:"http://bit.ly/1Uv0iJt"},
+    {name:"Salt Point", image:"http://bit.ly/1Uv0iJt"},
+    {name:"Pepper Ridge", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSPRncMF_1mX0P6gPhNgwEz1ZvWQg5DxIClHcHT028n9feGEvbxQ"},
+    {name:"Rosemary Lakes", image:"http://bit.ly/1Uv0iJt"},
+    {name:"Salt Point", image:"http://bit.ly/1Uv0iJt"},
+    {name:"Pepper Ridge", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSPRncMF_1mX0P6gPhNgwEz1ZvWQg5DxIClHcHT028n9feGEvbxQ"},
+    {name:"Rosemary Lakes", image:"http://bit.ly/1Uv0iJt"}
 ]
 
 
@@ -27,6 +32,21 @@ app.get("/", function(req, res) {
 //Shows all campgrounds
 app.get("/campgrounds", function(req, res){
     res.render("campgrounds", {campgrounds:campgrounds} )
+});
+
+//add a new campground
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image:image};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+
+});
+
+// takes you to new campground form
+app.get("/campgrounds/new", function(req, res){
+    res.render("new") 
 });
 
 // Tells Express to Listen on a specified PORT and IP. Call back prints message to console.
