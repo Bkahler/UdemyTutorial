13fefbe9c3fc567b6c813f3f8cb09b02f4b70d09
Bryan Kahler <bkahler3@gmail.com>
2016-01-10 21:14:02 +0000

    added post route

--- WebDevBooot/blogApp/app.js
+++ WebDevBooot/blogApp/app.js
@@ -36,8 +36,22 @@ app.get("/blogs", function(req, res){
   });
 });
 
-app.get("blogs/new", function(req, res) {
-    
+app.get("/blogs/new", function(req, res) {
+  res.render("new")
+});
+
+app.post('/blogs' , function(req, res){
+  Blog.create(req.body.blog, function(err,blog){
+    if (err){
+      console.log("Failed to save new blog entry...");
+      console.log(err);
+      res.render("new");
+    }
+    else{
+      console.log("New blog saved successfully")
+      res.redirect("/blogs");
+    };
+  });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
