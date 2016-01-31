bf24a9033328c32e61dbd0314e3e56835b06ae33
Bryan Kahler <bkahler3@gmail.com>
2016-01-31 02:39:22 +0000

    finished auth demo

--- WebDevBooot/authentication/app.js
+++ WebDevBooot/authentication/app.js
@@ -24,43 +24,72 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 
+var loginRedirect = { successRedirect: '/secret', failureRedirect: '/login'} ;
+
+////////////////////////////////
 //////////Routes///////////////
+//////////////////////////////
 
 app.get('/', function(req, res){
+    console.log('redering home...')
     res.render('home');
 });
 
-app.get('/secret', function(req, res) {
+app.get('/secret', isLoggedIn,function(req, res) {
+    console.log('rendering secret...')
     res.render('secret');
 });
 
+
+//////////////////////////////////////
 ////////// Auth Routes///////////////
+////////////////////////////////////
 app.get('/register', function(req, res) {
+    console.log('rendering register page ...')
     res.render('register');
 });
 
 app.post('/register', function(req, res) {
-    User.register(new User({username: req.body.username}), req.body.password, function(err,user){
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
         if(err){
             console.log(err);
             return res.render('register');
         };
         
         passport.authenticate('local')(req, res, function(){
+            console.log('successfully registered...')
             res.redirect('/secret');
         });
     });
 });
 
 app.get('/login', function(req, res) {
+    console.log('routeing to login page...')
     res.render('login');
 });
-
-var loginRedirect = { successRedirect: '/secret', failureRedirect: '/login'} ;
 
 app.post('/login', passport.authenticate('local', loginRedirect ), function(req, res) {
+    console.log('logged in successfully...')
     res.render('login');
 });
+
+app.get('/logout', function(req, res) {
+    req.logout();
+    console.log('successfully logged out..')
+    res.redirect('/');
+})
+
+
+
+/////////////////////
+/////middle ware////
+///////////////////
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    };
+    res.redirect('/login');
+};
 
 
 // Tells Express to Listen on a specified PORT and IP. Call back prints message to console.
