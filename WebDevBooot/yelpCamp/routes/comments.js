f4c67f8f0e4f78b4b66a11ece2628ad4c937f636
Bryan Kahler <bkahler3@gmail.com>
2016-02-13 19:56:59 +0000

    added edit functionality for comments

--- WebDevBooot/yelpCamp/routes/comments.js
+++ WebDevBooot/yelpCamp/routes/comments.js
@@ -47,4 +47,38 @@ router.post("/", isLoggedInMiddleware ,function(req, res) {
     });
 });
 
+//// EDIT Comments ////
+router.get("/:comment_id/edit", function(req, res){
+    var campgroundId = req.params.id
+    Comment.findById(req.params.comment_id, function(err,foundComment){
+        if(err){
+            console.log("error finding comment to edit...");
+            console.log(err);
+            res.redirect("back");
+        }
+        else{
+            res.render("comments/edit",{comment:foundComment, campgroundId:campgroundId});  
+        }
+    }); 
+});
+
+//// UPDATE Comments ////
+router.put("/:comment_id", function(req, res){
+    var commentId    = req.params.comment_id,
+        comment      = req.body.comment,
+        campgroundId = req.params.id;
+        
+    Comment.findByIdAndUpdate(commentId, comment, function(err, Updatedcomment){
+      if(err){
+          console.log("error updating comment...");
+          console.log(err);
+      }
+      else{
+        console.log("comment successfully updated");  
+        res.redirect("/campgrounds/" + campgroundId );   
+      }
+    })    
+});
+
+//// DELETE Comments ////
 module.exports = router;
