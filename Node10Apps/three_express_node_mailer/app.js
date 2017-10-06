879e464ca8bbcead58854f259ee3f25f0c6b4e7e
Bryan Kahler <bkahler3@gmail.com>
2017-10-06 11:02:35 -0700

    Finishing up NodeMailer Section

--- Node10Apps/three_express_node_mailer/app.js
+++ Node10Apps/three_express_node_mailer/app.js
@@ -15,12 +15,45 @@ app.use(bodyParser.urlencoded({extended: false}));
 //sets the public folder as the location for the assets 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', function(req, resp){
+app.get('/', function(res, resp){
   resp.render('index', {title: 'Welcome'});
 });
 
-app.get('/about', function(req, resp){
+app.get('/about', function(res, resp){
   resp.render('about');
+});
+
+app.get('/contact', function(res, resp){
+  resp.render('contact');
+});
+
+app.post('/contact/send', function(res, resp){
+  var transporter = nodeMailer.createTransport({
+    service: 'Gmail',
+    auth:{
+      user: '------------------',
+      pass: '------------------'
+    }
+  });
+
+  var mailOptions = {
+    from: 'Bryan Kahler <bkahler3@gmail.com>',
+    to: 'Bryan <bryan.kahler@autodesk.com>',
+    subject: 'Website Submission',
+    text: 'You have a Submission with the following details... Name: ' +res.body.name+ 'Email: ' +res.body.email+ 'Message: ' +res.body.message,
+    html: '<p>You have a Submission with the following details...</p><ul><li>Name: '+res.body.name+'</li><li>Email: '+res.body.email+'</li><li>Message: '+res.body.message+'</li></ul>'
+  }
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error){
+      console.log(error);
+      res.redirect('/');
+    } else{
+      console.log('Mail Sent Successfully: '+ info.response);
+      resp.render('index', {title: 'Welcome'});
+    };
+  });
+
 });
 
 app.listen(3000);
